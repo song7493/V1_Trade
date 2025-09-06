4edfa2840f77c3bdb7020d0472ce0c1fdfea1c63
song7493 <song7493@gmail.com>
2025-09-06 21:25:18 +0900

    feat(ui): align status clock to the right and unify time format

--- src/App/MainForm.cs
+++ src/App/MainForm.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using System.Windows.Forms;
 using V1_Trade.Infrastructure.UI;
 
@@ -5,9 +7,38 @@ namespace V1_Trade.App
 {
     public class MainForm : BaseForm
     {
+        private readonly StatusStrip _status = new();
+        private readonly ToolStripStatusLabel _spring = new() { Spring = true };
+        private readonly ToolStripStatusLabel _clockLabel = new();
+        private readonly Timer _timer = new();
+
         public MainForm()
         {
             Text = "V1 Trade (Baseline)";
+
+            _status.Items.Add(_spring);
+            _status.Items.Add(_clockLabel);
+            Controls.Add(_status);
+
+            _timer.Interval = 1000;
+            _timer.Tick += TimerTick;
+            _timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            _clockLabel.Text = DateTime.Now.ToString("yyyy-MM-dd dddd tt h:mm:ss");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _timer.Tick -= TimerTick;
+                _timer.Dispose();
+                _status.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
