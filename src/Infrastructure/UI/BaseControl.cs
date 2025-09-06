9b694ac21edabcfecbba3b5682a1c11b7a8f796c
song7493 <song7493@gmail.com>
2025-09-06 15:00:40 +0900

    Apply configurable global font

--- src/Infrastructure/UI/BaseControl.cs
+++ src/Infrastructure/UI/BaseControl.cs
@@ -1,36 +1,15 @@
-using System;
 using System.Windows.Forms;
 
 namespace V1_Trade.Infrastructure.UI
 {
     /// <summary>
-    /// Base user control that applies global font settings.
+    /// Base user control that applies global font settings once during construction.
     /// </summary>
     public class BaseControl : UserControl
     {
         public BaseControl()
         {
-            FontManager.Instance.FontChanged += OnFontChanged;
-        }
-
-        protected override void OnHandleCreated(EventArgs e)
-        {
-            base.OnHandleCreated(e);
-            FontManager.Instance.ApplyFontDeep(this);
-        }
-
-        private void OnFontChanged(object sender, EventArgs e)
-        {
-            FontManager.Instance.ApplyFontDeep(this);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                FontManager.Instance.FontChanged -= OnFontChanged;
-            }
-            base.Dispose(disposing);
+            FontManager.ApplyFont(this);
         }
     }
 }
