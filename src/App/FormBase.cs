eeec09bcea896c2ff6d63fa61366ac67c79a4dbf
song7493 <song7493@gmail.com>
2025-09-08 12:27:04 +0900

    feat: add form base and simplify font application

---
+++ src/App/FormBase.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+using V1_Trade.Infrastructure.UI;
+
+namespace V1_Trade.App
+{
+    /// <summary>
+    /// Base form that applies global font settings.
+    /// </summary>
+    public class FormBase : Form
+    {
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            FontManager.Apply(this);
+        }
+    }
+}
