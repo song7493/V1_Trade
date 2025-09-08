03cd28c82be38d067b1d030b3d734093765fab80
song7493 <song7493@gmail.com>
2025-09-08 13:24:38 +0900

    chore: remove obsolete loadsettings call

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Windows.Forms;
-using V1_Trade.Infrastructure.UI;
-
 namespace V1_Trade.App
 {
     static class Program
@@ -11,9 +9,6 @@ namespace V1_Trade.App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // Load persisted font settings before any forms are created.
-            FontManager.LoadSettings();
-
             Application.Run(new MainForm());
         }
     }
